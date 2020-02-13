b08e5fd94953923f32fb3531e90e469e77ac7207
Vladimir Teplov <vteplov@brightgrove.com>
2020-02-13 13:01:29 +0200

    add MageRegistry module

---
+++ app/code/MageMastery/FirstModule/Controller/Page/View.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MageMastery\FirstModule\Controller\Page;
+
+use Magento\Framework\Controller\Result\Json;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class View extends Action
+{
+    public function execute()
+    {
+        /** @var Json $jsonResult */
+        $jsonResult = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $jsonResult->setData(['message' => 'My First Page']);
+        return $jsonResult;
+    }
+}
---
+++ rebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy -f
+php bin/magento cache:clean
+php bin/magento cache:flush
+sudo chmod 777 -R var
+find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
+find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
+chown -R :www-data .
