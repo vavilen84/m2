0430cec55f9816db5d326078c9a011d97e5c836a
Vladimir Teplov <vteplov@brightgrove.com>
2020-02-13 15:34:10 +0200

    pass data from controller to block

--- app/code/MageMastery/FirstLayout/Controller/Page/View.php
+++ app/code/MageMastery/FirstLayout/Controller/Page/View.php
@@ -4,6 +4,8 @@ namespace MageMastery\FirstLayout\Controller\Page;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Result\Page;
 
 class View extends Action
 {
@@ -11,6 +13,13 @@ class View extends Action
 
     public function execute()
     {
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        /** @var Page $page */
+        $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+        /** @var Template $block */
+        $block = $page->getLayout()->getBlock('magemastery.first.layout.example');
+        $block->setData('message', 'Data from the Controller');
+
+        return $page;
     }
 }
